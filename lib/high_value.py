2429f3e8dc8c908fdacbd3dd014211637962bbbc
ZahedaS <Zaheda.shaikh@leics.police.uk>
2025-06-27 15:07:37 +0100

    Add design recipe file

--- lib/high_value.py
+++ lib/high_value.py
@@ -2,8 +2,12 @@
 
 class HighValue:
     def __init__(self, value_first, value_second):
+        if not isinstance(value_first, int) and not isinstance(value_second, int):
+            raise TypeError("Incorrect datatype")
+        
         self.value_first = value_first
         self.value_second = value_second
+        
     def get_highest(self):
         if self.value_first > self.value_second:
             return "First value is higher"
@@ -11,8 +15,18 @@ class HighValue:
             return "Second value is higher"
         else:
             return "Values are equal"
+        
     def add(self, increase_by, selection):
-        if selection == "first":
+         if selection == "first":
             self.value_first += increase_by
-        elif selection == "second":
+         elif selection == "second":
             self.value_second += increase_by
+         else:
+             return "Select first or second"
+
+    
+
+
+hv = HighValue(3,4)
+hv.add(3, "third")
+print(hv.get_highest())
